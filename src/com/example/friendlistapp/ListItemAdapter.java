154314705e8c63b2be4155bd57788ee79826d48d
rascalyen <Rascal22>
2013-08-05 14:12:54 -0700

    Updated: ListItemAdapter, listitem layout
    a. Add section header view for list row
    b. Delete SectionIndexer implementation

--- src/com/example/friendlistapp/ListItemAdapter.java
+++ src/com/example/friendlistapp/ListItemAdapter.java
@@ -1,8 +1,5 @@
 package com.example.friendlistapp;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import com.facebook.model.GraphUser;
 import com.squareup.picasso.Picasso;
@@ -12,7 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.SectionIndexer;
 import android.widget.TextView;
 
 /**
@@ -20,77 +16,71 @@ import android.widget.TextView;
  * 
  * @author Rascal
  */
-public class ListItemAdapter extends ArrayAdapter<GraphUser> implements SectionIndexer {
+public class ListItemAdapter extends ArrayAdapter<GraphUser> {
 	
 	private final Context context;
 	private final List<GraphUser> users;
-	private HashMap<String, Integer> alphas;
-    private String[] sections;
+    private String initHeader;
 
 	public ListItemAdapter(Context context, List<GraphUser> users) {
 		super(context, R.layout.listitem, users);
 		this.context = context;
 		this.users = users;
-		getMySections(users);
 	}
 
-	
-	private void getMySections(List<GraphUser> users) {
-		alphas = new HashMap<String, Integer>();
-        for (int i = users.size()-1 ; i >= 0; i--) 
-        		alphas.put(users.get(i).getName().substring(0, 1), i); 
-        
-        ArrayList<String> letters = new ArrayList<String>(alphas.keySet());
-        Collections.sort(letters);
-        sections = new String[letters.size()];
-        letters.toArray(sections);
-	}
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		ViewHolder holder;
-
+		
 		if (convertView == null) {
 			LayoutInflater inflater = (LayoutInflater) context
 					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 			convertView = inflater.inflate(R.layout.listitem, null);
-
 			holder = new ViewHolder();
-			holder.pictureView = (ImageView) convertView
-					.findViewById(R.id.list_profile_pic);
-			holder.nameView = (TextView) convertView
-					.findViewById(R.id.list_user_name);
+			initRowView(convertView, holder);
 			convertView.setTag(holder);
 		} else {
 			holder = (ViewHolder) convertView.getTag();
 		}
-		
-		String url = "http://graph.facebook.com/" + users.get(position).getId() + "/picture?type=large";
-		Picasso.with(context).load(url).resize(60, 60).centerCrop().into(holder.pictureView);
-		holder.nameView.setText(users.get(position).getName());
-
+		setRowView(position, holder);
 		return convertView;
 	}
 	
-	@Override
-	public int getPositionForSection(int section) {
-		return alphas.get(sections[section]);
-	}
-
-	@Override
-	public int getSectionForPosition(int position) {
-		return 1;
-	}
-
-	@Override
-	public Object[] getSections() {
-		return sections;
+	
+	private void initRowView(View convertView, ViewHolder holder) {
+		holder.pictureView = (ImageView) convertView
+				.findViewById(R.id.list_profile_pic);
+		holder.nameView = (TextView) convertView
+				.findViewById(R.id.list_user_name);
+		holder.headerView = (TextView) convertView
+				.findViewById(R.id.list_header);	
 	}	
 	
-
+	
+	private void setRowView(int position, ViewHolder holder) {
+		holder.nameView.setText(users.get(position).getName());
+		
+		String firstLetter = users.get(position).getName().substring(0, 1);
+		if (position == 0)
+			initHeader = "";
+		if (!initHeader.equals(firstLetter)) {
+			initHeader = firstLetter;
+			holder.headerView.setText(initHeader);
+			holder.headerView.setVisibility(View.VISIBLE);
+		} else {
+			holder.headerView.setVisibility(View.GONE);
+		}
+		
+		String url = "http://graph.facebook.com/" + users.get(position).getId() + "/picture?type=large";
+		Picasso.with(context).load(url).resize(60, 60).centerCrop().into(holder.pictureView);
+	}
+	
+	
 	private static class ViewHolder {
 		public ImageView pictureView;
 		public TextView  nameView;
+		public TextView  headerView;
 	}
 
 }
